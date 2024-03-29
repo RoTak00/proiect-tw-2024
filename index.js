3e07f0a1036b9b39375f249864dd085d3b80b7c9
Robert Takacs <RoTak@protonmail.com>
2024-03-29 10:22:21 +0200

    Finalizare etapa 4

--- index.js
+++ index.js
@@ -5,16 +5,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 const errorInfoPath = path.join(__dirname, "erori.json");
-let errorData;
+var obGlobal = { obErori: null };
 
-try {
-  const rawData = fs.readFileSync(errorInfoPath);
-  errorData = JSON.parse(rawData);
-} catch (error) {
-  console.error(
-    "Failed to read or parse the error information JSON file.",
-    error
-  );
+if (!initErori()) {
+  console.error("Nu se pot initializa erorile.");
+  process.exit(1);
 }
 
 const folders_to_create = ["temp", "temp1"];
@@ -48,7 +43,7 @@ app.use("/resurse", (req, res, next) => {
       next();
     } else if (stats.isDirectory()) {
       // Dacă calea este un director, returnează eroarea 403
-      displayError(res, 403);
+      afisareEroare(res, 403);
     } else {
       // Dacă nu este director, continuă cu restul middleware-urilor (posibil cel de servire statică a fișierelor)
       next();
@@ -59,8 +54,8 @@ app.use("/resurse", (req, res, next) => {
 app.use((req, res, next) => {
   if (req.path.endsWith(".ejs")) {
     // În acest caz, nu trimitem titlu, text sau imagine specifică,
-    // deci funcția displayError va folosi valorile predefinite pentru eroarea 400 din JSON
-    return displayError(res, 400);
+    // deci funcția afisareEroare va folosi valorile predefinite pentru eroarea 400 din JSON
+    return afisareEroare(res, 400);
   }
   next();
 });
@@ -102,7 +97,7 @@ app.get("/*", (req, res) => {
 
   fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
-      displayError(res, 404);
+      afisareEroare(res, 404);
     } else {
       res.render(
         `pagini/${page}`,
@@ -110,9 +105,9 @@ app.get("/*", (req, res) => {
         function (error, renderResult) {
           if (error) {
             if (error.message.startsWith("Failed to lookup view")) {
-              displayError(res, 404);
+              afisareEroare(res, 404);
             } else {
-              displayError(res, 500);
+              afisareEroare(res, 500);
             }
           } else {
             res.send(renderResult);
@@ -127,14 +122,14 @@ app.listen(port, () => {
   console.log(`Serverul rulează pe portul ${port}`);
 });
 
-function displayError(res, identifier, title, text, image) {
-  console.log(errorData);
-  let errorInfo = errorData.eroare_default;
+function afisareEroare(res, identifier, title, text, image) {
+  console.log(obGlobal.obErori);
+  let errorInfo = obGlobal.obErori.eroare_default;
   let status = 200; // Default HTTP status code
 
-  // If an identifier is provided, try to find a matching error in errorData.info_erori
+  // If an identifier is provided, try to find a matching error in obGlobal.obErori.info_erori
   if (identifier) {
-    const foundError = errorData.info_erori.find(
+    const foundError = obGlobal.obErori.info_erori.find(
       (error) => error.identificator === identifier
     );
     if (foundError) {
@@ -150,9 +145,7 @@ function displayError(res, identifier, title, text, image) {
   let outputErrorInfo = Object.assign({}, errorInfo);
   outputErrorInfo.titlu = title || errorInfo.titlu;
   outputErrorInfo.text = text || errorInfo.text;
-  outputErrorInfo.imagine = image
-    ? path.join(errorData.cale_baza, image)
-    : path.join(errorData.cale_baza, errorInfo.imagine);
+  outputErrorInfo.imagine = image ? image : errorInfo.imagine;
 
   // Render the error page
   res.status(status).render("pagini/eroare", {
@@ -160,4 +153,26 @@ function displayError(res, identifier, title, text, image) {
     text: outputErrorInfo.text,
     imagine: outputErrorInfo.imagine,
   });
+}
+
+// FUNCTII
+
+function initErori() {
+  try {
+    const rawData = fs.readFileSync(errorInfoPath);
+    obGlobal.obErori = JSON.parse(rawData);
+
+    obGlobal.obErori.info_erori.forEach((error) => {
+      error.imagine = path.join(obGlobal.obErori.cale_baza, error.imagine);
+    });
+
+    console.log("Erorile au fost incarcate cu succes.", obGlobal.obErori);
+    return true;
+  } catch (error) {
+    console.error(
+      "Failed to read or parse the error information JSON file.",
+      error
+    );
+    return false;
+  }
 }
