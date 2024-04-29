6b9106efede3ef2a306fa7e4520866002cc6be7f
Robert Takacs <RoTak@protonmail.com>
2024-04-30 01:37:35 +0300

    Sortare, resetare si calculare

--- resurse/js/cursuri-filtering.js
+++ resurse/js/cursuri-filtering.js
@@ -69,7 +69,142 @@ $(document).ready(function () {
 
     filterCoursesByFilter();
   });
+
+  // sortare
+
+  $("#button-sort").on("click", function () {
+    let current_state = $(this).attr("data-sort");
+
+    if (current_state == "asc") {
+      current_state = "desc";
+      $(this).find("i").removeClass("fa-angles-up").addClass("fa-angles-down");
+    } else {
+      current_state = "asc";
+      $(this).find("i").removeClass("fa-angles-down").addClass("fa-angles-up");
+    }
+
+    $(this).attr("data-sort", current_state);
+    sortCourses(current_state);
+  });
+
+  $(document).on("click", "#button-reset", function () {
+    $("#modal-reset")[0].showModal();
+  });
+
+  $(document).on("click", "#reset-close", function () {
+    $("#modal-reset")[0].close();
+  });
+
+  $(document).on("click", "#reset-confirm", function () {
+    $("#modal-reset")[0].close();
+    filters = [];
+    filterCoursesByFilter();
+
+    // resetare
+
+    $(
+      ".filter-element input[type='text'], .filter-element input[type='number'], .filter-element input[type='range'], .filter-element textarea"
+    ).val("");
+    $(".filter-element input[type='checkbox']").prop("checked", false);
+    $(".filter-element input[type='radio']").prop("checked", false);
+    $(".filter-element select").val("");
+
+    // fixare
+    let min_price = $("#filter_max_price").attr("min");
+    let max_price = $("#filter_max_price").attr("max");
+
+    $("#filter_min_price").val(min_price);
+    $("#filter_max_price").val(max_price);
+    $("#fromInput").val(min_price);
+    $("#toInput").val(max_price);
+    $("#filter_rating_all").prop("checked", true);
+
+    if ($("#button-sort").attr("data-sort") == "desc") {
+      $("#button-sort")
+        .find("i")
+        .removeClass("fa-angles-down")
+        .addClass("fa-angles-up");
+      $("#button-sort").attr("data-sort", "asc");
+      sortCourses("asc");
+    }
+  });
+
+  $(document).on("click", "#button-calculate", function () {
+    $("#modal-calculate strong").text(calculateOpenCoursesPrice() + " RON");
+    $("#modal-calculate")[0].showModal();
+
+    setTimeout(function () {
+      $("#modal-calculate")[0].close();
+    }, 3000);
+  });
+
+  $(document).on("click", "#calculate-close", function () {
+    $("#modal-calculate")[0].close();
+  });
 });
+
+function calculateOpenCoursesPrice() {
+  let price = 0;
+
+  let courseList = $(".course");
+
+  courseList.each(function () {
+    let course = $(this);
+
+    let priceElement = course.find('[data-element="pret"]');
+    let priceValue = priceElement.data("key");
+
+    price += parseInt(priceValue);
+  });
+
+  return price;
+}
+
+function sortCourses(current_state) {
+  let courses_wrapper = $(".grid-courses");
+
+  let courses = courses_wrapper.children(".course");
+
+  courses
+    .sort(function (a, b) {
+      let price_a = $(a).find('[data-element="pret"]').data("key");
+      let price_b = $(b).find('[data-element="pret"]').data("key");
+
+      let ascdesc = current_state === "asc" ? 1 : -1;
+
+      if (price_a * ascdesc > price_b * ascdesc) {
+        return 1;
+      }
+
+      if (price_a * ascdesc < price_b * ascdesc) {
+        return -1;
+      }
+
+      let name_a = $(a).find('[data-element="nume"]').text();
+      let name_b = $(b).find('[data-element="nume"]').text();
+
+      if (ascdesc == 1 && name_a > name_b) {
+        return 1;
+      }
+
+      if (ascdesc == -1 && name_a < name_b) {
+        return 1;
+      }
+
+      if (ascdesc == 1 && name_a < name_b) {
+        return -1;
+      }
+
+      if (ascdesc == -1 && name_a > name_b) {
+        return -1;
+      }
+
+      return 0;
+    })
+    .each(function () {
+      courses_wrapper.append(this);
+    });
+}
 
 function filterCoursesByFilter() {
   console.log(filters);
@@ -145,6 +280,18 @@ function filterCoursesByFilter() {
             ) {
               return false;
             }
+            break;
+
+          case "luna_start_special":
+            if (
+              new Date(target_element.attr("datetime")) >
+              new Date(
+                new Date().getFullYear(),
+                new Date().getMonth() + 1,
+                new Date().getDate()
+              )
+            )
+              return false;
             break;
 
           case "rating":
